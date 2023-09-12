d9b1a83f574a4170b5f21d47cc6c9f5fc83f4a1d
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-09-12 14:06:16 +0200

    all aphabets

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
