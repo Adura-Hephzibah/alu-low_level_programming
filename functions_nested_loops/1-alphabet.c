b18cea9225949fb441b5443e0f70fbf31753d994
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-09-20 12:27:27 +0200

    betty

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- *main - prints the alphabets, followed by new line
+ *print_alphabet - prints the alphabets, followed by new line
  *
  *Return: 0
  */
 void print_alphabet(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
 }
