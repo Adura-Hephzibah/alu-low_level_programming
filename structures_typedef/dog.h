d7cf3a053eac45ac1cb2edc774587ecb3caa723e
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-10-16 14:34:03 +0200

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is a structure for dogs
+ * @name: first member of struct
+ * @age: second member of struct
+ * @owner: third member of struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
